151c56a8ffccaeee642c1716adc5ffaf0feb5067
Leonard <leonardposner@googlemail.com>
2017-01-21 20:49:21 +0100

    Zustand Emission added

--- src/hmmmodel/Emission.java
+++ src/hmmmodel/Emission.java
@@ -4,4 +4,20 @@ package hmmmodel;
  * Created by anthony on 21.01.17.
  */
 public class Emission {
+
+    int represent;
+    double prob;
+    Zustand owner;
+
+
+    public Emission(int represent, double prob, Zustand owner){
+        this.represent = represent;
+        this.prob = prob;
+        this.owner = owner;
+    }
+
+
+
+
+
 }
